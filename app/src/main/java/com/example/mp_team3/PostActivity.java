adb1a4eff3244323eab421faf972206e58aa31e6
e50hxxn <e50hxxn@gmail.com>
2022-11-15 20:41:24 +0900

    Rename project

---
+++ app/src/main/java/com/example/mp_team3/PostActivity.java
@@ -0,0 +1,16 @@
+package com.example.mp_team3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat_post);
+    }
+}
