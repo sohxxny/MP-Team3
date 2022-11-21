234c8f2b168e329013bb2d3f9837d6e93dda5229
EunJin <eunjin42767@gmail.com>
2022-11-21 15:06:09 +0900

    ---2022/11/21(이미지버튼 변경 / 버튼클릭이슈)---

--- app/src/main/java/com/example/mp_team3/Fragment_home.java
+++ app/src/main/java/com/example/mp_team3/Fragment_home.java
@@ -8,25 +8,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Fragment_home extends Fragment {
+    View view;
+    FloatingActionButton createPost;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
+
+        view = inflater.inflate(R.layout.fragment_home,container,false);
+        createPost = (FloatingActionButton) view.findViewById(R.id.fabCreatePost);
+
+        createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PostActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.creatPost:
-                Intent intent = new Intent(getActivity(), PostActivity.class);
-                startActivity(intent);
-                break;
-        }
-    }
 
 
 }
