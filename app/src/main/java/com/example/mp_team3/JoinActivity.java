35b1aebc2cb83d5a3368dc7ae86068c6ae26d49a
e50hxxn <e50hxxn@gmail.com>
2022-11-21 15:00:42 +0900

    ---2022/11/21(로그인 수정 및 회원가입 액티비티 추가)---

---
+++ app/src/main/java/com/example/mp_team3/JoinActivity.java
@@ -0,0 +1,14 @@
+package com.example.mp_team3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
