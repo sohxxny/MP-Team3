5dd1fe1bb794ef30d5980db63e4f6da651a2a80d
EunJin <eunjin42767@gmail.com>
2022-12-02 20:02:20 +0900

    ---2022/12/02(프로필 사진 변경)---

--- app/src/main/java/com/example/mp_team3/Fragment_my.java
+++ app/src/main/java/com/example/mp_team3/Fragment_my.java
@@ -1,5 +1,6 @@
 package com.example.mp_team3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class Fragment_my extends Fragment {
+
+    View view;
+    ImageButton btnMyEdit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my, container, false);
+        view = inflater.inflate(R.layout.fragment_my, container, false);
+        btnMyEdit = (ImageButton) view.findViewById(R.id.btnMyEdit);
+
+        btnMyEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProfileEdit.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
     }
 }
--- app/src/main/java/com/example/mp_team3/PostActivity.java
+++ app/src/main/java/com/example/mp_team3/PostActivity.java
@@ -3,14 +3,18 @@ package com.example.mp_team3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PostActivity extends AppCompatActivity {
 
-
+    Button btnPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_post);
+
+        btnPicture = (Button) findViewById(R.id.btnPicture);
+
     }
 }
