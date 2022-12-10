ac5f1f67713ff8f712fa8c18d92139d20b45bc91
e50hxxn <e50hxxn@gmail.com>
2022-12-11 06:18:41 +0900

    ---2022/12/11(검색창 기능 수정)---

--- app/src/main/java/com/example/mp_team3/Fragment_home.java
+++ app/src/main/java/com/example/mp_team3/Fragment_home.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.lang.reflect.Array;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 public class Fragment_home extends Fragment {
     View view;
     FloatingActionButton createPost;
+    AppCompatButton btnGotoSearch;
     RecyclerView homeRecycler;
     ArrayList<String[]> pList;
     ArrayList<Uri> iList;
@@ -30,6 +34,7 @@ public class Fragment_home extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home,container,false);
         createPost = (FloatingActionButton) view.findViewById(R.id.fabCreatePost);
+        btnGotoSearch = (AppCompatButton) view.findViewById(R.id.btnGotoSearch);
 
         //
         homeRecycler = (RecyclerView) view.findViewById(R.id.homeRecycler);
@@ -44,6 +49,15 @@ public class Fragment_home extends Fragment {
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), PostActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        // 검색창 누르면 검색 페이지로 이동
+        btnGotoSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Search.class);
                 startActivity(intent);
             }
         });
